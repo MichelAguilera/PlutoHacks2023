9a8ec5ff23c08a07e30b54e9160058c2a6f595d2
Michel Aguilera <michelaguilera01@gmail.com>
2023-10-14 06:17:37 -0400

    0.3.0

--- Scraper/api_handler.py
+++ Scraper/api_handler.py
@@ -1,19 +1,28 @@
 # Get the API keys
 import json
+import requests
+import openai
+from rich.console import Console
+c = Console()
 
-json_f = open("api_keys.json")
-api_keys = json.load(json_f)
-json_f.close()
+json_f_keys = open("api_keys.json")
+api_keys = json.load(json_f_keys)
+json_f_keys.close()
+
+json_f_urls = open("endpoints.json")
+endpoints = json.load(json_f_urls)
+json_f_urls.close()
 
 # The Guardian
 from theguardian import theguardian_content
-def get_guardian() -> str:
-    query = "Microsoft"
+def get_guardian(a_query) -> str:
+    query = a_query
     date = 1 # CHANGE THIS
     headers = {
         "q": query,
-        # "tag": "film/film,tone/reviews",
-        "from-date": date,
+        "query-fields": "headline",
+        # "tag": "technology",
+        # "from-date": date,
         "order-by": "relevance",
         "show-fields": "headline",
     }
@@ -28,4 +37,46 @@ def parse_json_guardian(json_content):
     for result in json_content["response"]["results"]:
         results.append(result["fields"]["headline"])
 
+    c.log(f"GUARDIAN RESULTS {results}")
     return results
+
+def get_bing(a_query):
+    query = a_query
+    date = 1 # CHANGE THIS
+    headers = {
+        "Ocp-Apim-Subscription-Key": api_keys["Azure"],
+    }
+    params = {
+        "q": query
+        # "textDecorations": True, 
+        # "textFormat": "HTML"
+    }
+    content = requests.get(endpoints["Bing"], headers=headers, params=params)
+    content.raise_for_status()
+    json_content = content.json()
+    headlines = parse_json_bing(json_content)
+
+    return headlines
+
+def parse_json_bing(json_content):
+    print(json_content)
+    
+    results = []
+    for result in json_content["response"]["results"]:
+        results.append(result["fields"]["headline"])
+
+    c.log(f"GUARDIAN RESULTS:\n{results}")
+    return results
+
+def get_openai(prompt):
+    openai.organization = api_keys["OpenAIorg"]
+    openai.api_key = api_keys["OpenAI"]
+    response = openai.Completion.create(
+        engine="gpt-3.5-turbo-instruct", 
+        prompt=prompt, 
+        max_tokens=300
+    )
+
+    result = response['choices'][0]['text'].strip()
+    c.log(f"AI RESPONSE:\n{result}")
+    return result
--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-print("Hello World!")
+from Scraper.api_handler import get_guardian, get_bing, get_openai
 
-from Scraper.api_handler import get_guardian
+def main():
+    query = input("Search for: ")
 
-print(get_guardian())
+    headlines = get_headlines(query)
+    response = get_ai_response(headlines)
+    # print(response)
+
+def get_headlines(query):
+    return get_guardian(query)
+
+def list_to_string(headlines):
+    headlines_string = ""
+    for headline in headlines:
+        headlines_string += "* " + headline + "\n"
+    return headlines_string
+
+def get_ai_response(headlines):
+    headlines = list_to_string(headlines)
+    
+    BOILERPLATE = f"This is a list of recent headlines about a specific company, please analyze them and give me advice on whether it is currently safe to invest in this company based on its media coverage.\n{headlines}\nBe professional and concise, keep your answers to one or two paragraphs."
+
+    response = get_openai(BOILERPLATE)
+    return response
+
+if __name__ == "__main__":
+    main()
